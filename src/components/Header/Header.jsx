208697ae5245f7fcea7111c2e1baf7609642ebe1
BillelBenoudjit <hb_benoudjit@esi.dz>
2022-03-07 16:55:47 +0100

    Add models details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,6 +42,28 @@ function Header({ text, setText, setNamedEntities, model, setModel }) {
             : alert("Modèle indisponible!")
     }
 
+    const displayModelDetails = (model) => {
+        switch (model) {
+            case "Camembert/DEFT - FR":
+                return "CamemBERT est un modèle linguistique de pointe pour le français basé sur le modèle RoBERTa. Il a été affiné en utilisant le"
+                    + " dataset CAS du challege DEFT 2020 avec 717 fichiers et 12150 annotations."
+            case "Bio_ClinicalBert/NCBI - EN":
+                return "Le modèle Bio_ClinicalBERT a été entraîné sur toutes les notes de MIMIC III en se baseant sur BERT. Il a été affiné en utilisant le"
+                    + " le NCBI National Center of Bio Technology Information, un ensemble de données sur les mentions de noms de maladies de 793 résumés PubMed."
+            case "BlueBert/NCBI - EN":
+                return "Un modèle BERT pré-entraîné sur les résumés PubMed et les notes cliniques (MIMIC-III). Il a été affiné en utilisant le NCBI -"
+                    + " National Center of Bio Technology Information, un ensemble de données sur les mentions de noms de maladies de 793 résumés PubMed."
+            case "BlueBert/BC5CDR - EN":
+                return "Un modèle BERT pré-entraîné sur les résumés PubMed et les notes cliniques (MIMIC-III). Il a été affiné en utilisant le corpus BC5CDR"
+                    + " qui se compose de 1500 articles PubMed avec 4409 produits chimiques annotés, 5818 maladies et 3116 interactions produit chimique-maladie."
+            case "BlueBert/BioNLP13CG - EN":
+                return "Un modèle BERT pré-entraîné sur les résumés PubMed et les notes cliniques (MIMIC-III). Il a été affiné en utilisant le corpus BioNLP13CG."
+                    + " contenant 16 annotations réliés au domaine médicale."
+            default:
+                return ""
+        }
+    }
+
     return (
         <div className="header">
             <div className="inputs">
@@ -62,6 +84,7 @@ function Header({ text, setText, setNamedEntities, model, setModel }) {
                     </Input>
                     <Button style={{ display: "inline" }} onClick={handleSubmit}>Soumettre</Button>
                 </Form>
+                <p>{displayModelDetails(model)}</p>
             </div>
         </div>
     )
